b92560f121e0754792427de47cffcc35f01d32c5
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2023-05-12 01:58:14 +0530

    Time: 24 ms (48.97%) | Memory: 13 MB (64.14%) - LeetSync

--- uncrossed-lines/uncrossed-lines.cpp
+++ uncrossed-lines/uncrossed-lines.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
 
-    int findM(int i, int j, vector<int>& nums1, vector<int>& nums2, vector<vector<int>> &dp){
-        if((i<0 && j>=0) || (j<0 && i>=0) || (i<0 && j<0))  return 0;
-        if(dp[i][j] != -1)  return dp[i][j];
+    // int findM(int i, int j, vector<int>& nums1, vector<int>& nums2, vector<vector<int>> &dp){
+    //     if((i<0 && j>=0) || (j<0 && i>=0) || (i<0 && j<0))  return 0;
+    //     if(dp[i][j] != -1)  return dp[i][j];
 
-        if(nums1[i] == nums2[j])     
-            return  dp[i][j] =  1 + findM(i-1, j-1, nums1, nums2, dp);
-        else        
-            return dp[i][j] =  max(findM(i-1, j, nums1, nums2, dp) , findM(i, j-1, nums1, nums2, dp));
-    }
+    //     if(nums1[i] == nums2[j])     
+    //         return  dp[i][j] =  1 + findM(i-1, j-1, nums1, nums2, dp);
+    //     else        
+    //         return dp[i][j] =  max(findM(i-1, j, nums1, nums2, dp) , findM(i, j-1, nums1, nums2, dp));
+    // }
 
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
 
         if(nums1 == nums2)  return nums1.size();
 
-        vector<vector<int>> dp(nums1.size(), vector<int>(nums2.size(), -1));
+        vector<vector<int>> dp(nums1.size()+1, vector<int>(nums2.size()+1));
 
-        return findM(nums1.size()-1, nums2.size()-1, nums1, nums2, dp);
+        int n = nums1.size(), m = nums2.size();
+
+        for(int i = 0 ; i<=n; i++)  dp[i][0] = 0;
+        for(int j = 0 ; j<=m; j++)  dp[0][j] = 0;
+
+        for(int i = 1; i<=n; ++i){
+            for(int j = 1; j<=m; ++j){
+                if(nums1[i-1] == nums2[j-1])    dp[i][j] = 1+ dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
 
     }
 };
