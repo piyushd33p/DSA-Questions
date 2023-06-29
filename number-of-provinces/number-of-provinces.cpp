d90e61e730504a685068ce96f2eba63205c49864
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2023-06-29 21:19:52 +0530

    Time: 29 ms (49.30%) | Memory: 15.4 MB (16.02%) - LeetSync

--- number-of-provinces/number-of-provinces.cpp
+++ number-of-provinces/number-of-provinces.cpp
@@ -1,42 +1,56 @@
+class DisjointSet{
+    vector<int> parent, size;
 
-class Solution {
-
-private:
-    void dfs(vector<int> adj[], vector<bool> &vis, int node){
-        vis[node] = 1;
-
-        for(auto i : adj[node]){
-            if(!vis[i])  
-                dfs(adj, vis, i);
+    public:
+    DisjointSet(int n){
+        size.resize(n+1, 1);
+        parent.resize(n+1);
+        for(int i = 0 ; i< n; i++){
+            parent[i] = i;
         }
     }
+
+    int findPar(int node){
+        if(node == parent[node])    return node;
+
+        return parent[node] = findPar(parent[node]);
+    }
+
+
+    void unionBySize(int u, int v){
+        int ulp_u = findPar(u);
+        int ulp_v = findPar(v);
+
+        if(ulp_u == ulp_v)  return;
+        else if(size[ulp_u]  < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
+        DisjointSet ds(n);
 
-        vector<int> adj[n];
-
-        for(int i = 0 ; i < n; i++){
-            for(int j = 0 ; j < n ; j++){
-
-                if(isConnected[i][j] == 1 && i != j){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+        for(int i =0  ; i < n ; i++){
+            for(int j = 0 ; j<n;j++){
+                if(isConnected[i][j] == 1){
+                    ds.unionBySize(i,j);
                 }
             }
         }
+        int count = 0;
 
-        vector<bool> vis(n,0);
-
-        int numProvince = 0;
-
-        for(int i = 0 ; i < n; i++){
-            if(!vis[i]){
-                numProvince++;
-                dfs( adj, vis, i);
-            }
+        for(int i = 0; i<n; i++){
+            if(ds.findPar(i) == i)  count++;
         }
 
-        return numProvince;
+        return count;
     }
 };
