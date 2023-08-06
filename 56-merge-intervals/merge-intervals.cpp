f7e34f40f9e8b647c02a3fd4e62905f5c618355d
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2023-08-06 23:11:48 +0530

    Time: 28 ms (95.96%) | Memory: 18.9 MB (70.21%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -7,22 +7,12 @@ public:
         int n = intervals.size();
 
         for(int i = 0 ; i < n ; i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if(!ans.empty() && end <= ans.back()[1]){
-                continue;
+            if(ans.empty() ||  intervals[i][0] > ans.back()[1]){
+                ans.push_back({intervals[i][0], intervals[i][1]});
             }
-
-            for(int j = i+1; j < n; j++){
-                if( intervals[j][0] <= end){
-                    end = max(end, intervals[j][1]);
-                }
-                else    
-                    break;
+            if(intervals[i][0] <= ans.back()[1]){
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-
-            ans.push_back({start, end});
         }
         return ans;
     }
