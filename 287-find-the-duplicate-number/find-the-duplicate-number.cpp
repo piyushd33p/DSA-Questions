63aabdbe92ab8d68b86f407c03a5b62a8edced3e
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2023-08-07 02:45:39 +0530

    Time: 126 ms (52.90%) | Memory: 61.2 MB (40.22%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -2,15 +2,44 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int n = nums.size();
-        map<int,int> mp;
-
-        for(auto it : nums){
-            mp[it]++;
-            if(mp[it] > 1)  return it;
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for(int i = 0 ; i < n-1 ; i++){
+            if(nums[i+1] == nums[i]){
+                ans = nums[i];
+                break;
+            }
         }
-        return -1;
+
+        return ans;
+
+        // int n = nums.size();
+        
+        // sort(nums.begin(), nums.end());
+
+        // int low = 1, high = n;
+
+        // while(low <= high){
+        //     int mid = low + (high - low)/2;
+        //     if((nums[mid] == nums[mid+1] && mid+1 <= n)) {
+        //         return nums[mid];
+        //     }
+        //     if(nums[mid] == nums[mid-1] && mid-1 >=0)  return nums[mid];
+        //     else if(mid == nums[mid])   low = mid+1;
+        //     else    high = mid-1;
+        // }
+        // return -1;
     }
 };
+
+
+// map<int,int> mp;
+
+//         for(auto it : nums){
+//             mp[it]++;
+//             if(mp[it] > 1)  return it;
+//         }
+//         return -1;
 
 // sort(nums.begin(), nums.end());
         // int ans = 0;
