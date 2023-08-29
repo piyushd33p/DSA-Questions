7715aa8fc3327620db43e690bbbc6e1fd5e95d40
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2023-08-29 14:30:02 +0530

    Time: 3 ms (65.10%) | Memory: 8 MB (39.14%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,30 +1,30 @@
 class Solution {
 private:
-    void subSum(int index, vector<int>& nums, vector<int>& ds, int n, set<vector<int>>& st){
+    void getSub(vector<int>& nums, int n, int index, set<vector<int>>& st, vector<int>& temp){
         if(index == n){
-            st.insert(ds);
+            st.insert(temp);
             return;
         }
-        ds.push_back(nums[index]);
 
-        subSum(index+1, nums, ds, n, st);
+        temp.push_back(nums[index]);
+        getSub(nums, n, index+1, st, temp);
 
-        ds.pop_back();
-        subSum(index+1, nums, ds, n, st);
-
+        temp.pop_back();
+        getSub(nums, n, index+1, st, temp);
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> ds;
-        int n = nums.size();
-
         set<vector<int>> st;
+        int n = nums.size();
+        int index = 0;
+        vector<int> temp;
         sort(nums.begin(), nums.end());
-        subSum(0, nums, ds, n, st);
+        getSub(nums, n, index, st, temp);
 
         vector<vector<int>> ans;
-
-        for(auto it:st)     ans.push_back(it);
+        for(auto it: st){
+            ans.push_back(it);
+        }
 
         return ans;
     }
