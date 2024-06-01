69a9967831b303a240fbb177109993f5f31e9215
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2024-06-01 17:46:49 +0530

    Time: 7 ms (83.32%) | Memory: 16.5 MB (26.98%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
+        
+        int col0 = 1;
+        int n = mat.size();
+        int m = mat[0].size();
 
-        vector<pair<int,int>> zero;
-
-        for(int i = 0 ; i< n ; i++){
+        for(int i = 0; i< n; i++){
             for(int j = 0 ; j < m ; j++){
-                if(mat[i][j] == 0){
-                    pair<int,int> p = make_pair(i , j);
-                    zero.push_back(p);
+                if(j == 0 && mat[i][j] == 0){
+                    col0 = 0;
                 }
+                else if(mat[i][j] == 0){
+                    mat[0][j] = 0;
+                    mat[i][0] = 0;
+                }
+                
+            }
+        }
+        for(int i = n-1; i > 0; i--){
+            for(int j = m-1; j >0; j--){
+                if(mat[0][j] == 0 || mat[i][0]== 0)
+                    mat[i][j] = 0;
             }
         }
 
-        for(auto it : zero){
-            int ptr1 = 0, ptr2 = 0, zRow = it.first, zCol = it.second ;
-
-            while(ptr1 < n){
-                mat[ptr1][zCol] = 0;
-                ptr1++;
+        //for zerorow
+        for(int i = m-1; i > 0 ; i--){
+            if(mat[0][i] == 0 || mat[0][0] == 0){
+                mat[0][i] = 0;
             }
+        }
 
-            while(ptr2 < m){
-                mat[zRow][ptr2] = 0;
-                ptr2++;
+        //for zerocol
+        for(int i = n-1; i>=0; i--){
+            if(mat[i][0] == 0 || col0 == 0 ){
+                mat[i][0] = 0;
             }
         }
     }
-
 };
