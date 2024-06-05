61481444effcb20a8bbe4882f2d304f4394897e6
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2024-06-05 14:39:43 +0530

    Time: 7 ms (34.57%) | Memory: 12.6 MB (82.33%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 private:
-    void find(vector<int>& nums, int target, int n, vector<vector<int>>& ans, vector<int>& temp, int index){
+    void find(vector<int>& nums, vector<vector<int>>& ans, vector<int>& ds, int target, int index){
         if(target == 0){
-            ans.push_back(temp);
+            ans.push_back(ds);
             return;
         }
 
-        for(int i = index; i < n; i++){
+        for(int i = index; i < nums.size(); i++){
             if(nums[i] > target)    break;
-            if(i > index && nums[i] == nums[i-1])   continue;
 
-            temp.push_back(nums[i]);
-            find(nums, target - nums[i],n, ans, temp, i+1);
-            temp.pop_back();
+            if( i > index && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            ds.push_back(nums[i]);
+            find(nums, ans, ds, target - nums[i], i+1);
+            ds.pop_back();
         }
-        
-
-
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        vector<int> temp;
-
         vector<vector<int>> ans;
-        find(nums, target, nums.size(), ans, temp, 0);
+        vector<int> ds;
+
+        sort(nums.begin(), nums.end());
+
+        find(nums, ans, ds, target, 0);
 
         return ans;
     }
