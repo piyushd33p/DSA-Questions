40da2aaca1dd33b292ead27fb42bfb8a9e13abd8
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2024-06-10 17:01:25 +0530

    Time: 76 ms (67.23%) | Memory: 63.5 MB (76.02%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,53 +1,25 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int ans = 0;
-        for(int i = 0 ; i < n-1 ; i++){
-            if(nums[i+1] == nums[i]){
-                ans = nums[i];
-                break;
-            }
-        }
-
-        return ans;
-
-        // int n = nums.size();
         
-        // sort(nums.begin(), nums.end());
+         int slow = nums[0];
+         int fast = nums[0];
+        
 
-        // int low = 1, high = n;
+        do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
 
-        // while(low <= high){
-        //     int mid = low + (high - low)/2;
-        //     if((nums[mid] == nums[mid+1] && mid+1 <= n)) {
-        //         return nums[mid];
-        //     }
-        //     if(nums[mid] == nums[mid-1] && mid-1 >=0)  return nums[mid];
-        //     else if(mid == nums[mid])   low = mid+1;
-        //     else    high = mid-1;
-        // }
-        // return -1;
+        }while(slow != fast);
+        
+        fast = nums[0];
+
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+
+        
     }
 };
-
-
-// map<int,int> mp;
-
-//         for(auto it : nums){
-//             mp[it]++;
-//             if(mp[it] > 1)  return it;
-//         }
-//         return -1;
-
-// sort(nums.begin(), nums.end());
-        // int ans = 0;
-        // for(int i = 1 ; i < n ; i++){
-        //     if(nums[i] == nums[i+1]){
-        //         ans = nums[i];
-        //         break;
-        //     }
-        // }
-
-        // return ans;
