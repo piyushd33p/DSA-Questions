bffe667372b48489806ddbcd77fc3f63711fe865
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2024-06-20 18:24:23 +0530

    Time: 17 ms (59.59%) | Memory: 26.7 MB (46.85%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count = 0;
-        int element = 0;
+        int n = nums.size();
 
-        for(auto it: nums){
-            if(count == 0) {
+        int a = INT_MIN, cnt1 = 0;
+        int b = INT_MIN, cnt2 = 0;
 
-            element = it;
-            count++;
-            } 
-            else if(it == element)  count++;
-            else count--;
+        for(int i= 0; i < n ;i++){
+            if(a == INT_MIN && nums[i] != b){
+                a = nums[i];
+                cnt1 = 1;
+            }
+            else if(b == INT_MIN && nums[i] != a){
+                b = nums[i];
+                cnt2 = 1;
+            }
+            else if(nums[i] == a)   cnt1++;
+            else if(nums[i] == b)   cnt2++;
+            else if(nums[i] != a && nums[i] != b){
+                if(cnt1 <= cnt2 ){
+                    a = nums[i];
+                    cnt1 = 1;
+                }
+                else{
+                    b = nums[i];
+                    cnt2 = 1;
+                }
+            }
         }
-        return element;
+        cout<< cnt1 << "   "<< cnt2;
+        if(cnt1 <= cnt2)    return b;
+        else    return a;
     }
 };
