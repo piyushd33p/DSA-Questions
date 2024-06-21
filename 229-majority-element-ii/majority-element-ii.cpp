5e7fc6585584cfc2e8571007bbb391f147df16af
piyushd33p <102989428+piyushd33p@users.noreply.github.com>
2024-06-21 11:30:32 +0530

    Time: 9 ms (57.99%) | Memory: 18.4 MB (44.89%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int n1 = INT_MIN, cnt1 = 0;
+        int n2 = INT_MIN, cnt2 = 0;
         int n = nums.size();
 
-        int ele1 = INT_MIN, cnt1 = 0;
-        int ele2 = INT_MIN, cnt2 = 0;
-        vector<int> ans;
-
-        for(auto it: nums){
-            if(cnt1 == 0  && it!= ele2){
-                ele1 = it;
-                cnt1++;
+        for(auto it : nums){
+            if(cnt1 == 0 && it != n2)
+            {
+                n1 = it;
+                cnt1 = 1;
             }
-            else if(cnt2 == 0 && it!= ele1){
-                ele2 = it;
-                cnt2++;
+            else if(cnt2 == 0 && it != n1){
+                n2 = it;
+                cnt2 = 1;
             }
-            else if(it == ele1)   cnt1++;
-            else if(it == ele2)   cnt2++;
-
+            else if(it == n1)   cnt1++;
+            else if(it == n2)   cnt2++;
             else{
                 cnt1--;
                 cnt2--;
             }
         }
 
-        cnt1 = 0, cnt2 = 0;
+        cnt1 = 0;
+        cnt2 = 0;
 
-        for(auto  it:nums){
-            if(it == ele1)  cnt1++;
-            else if(it == ele2) cnt2++;
+        vector<int> ans;
+
+        for(auto it: nums){
+            if(it == n1)    cnt1++;
+            if(it == n2)    cnt2++;
+
         }
 
-        if(cnt1 > n/3)     ans.push_back(ele1);
-        if(cnt2 > n/3)     ans.push_back(ele2);
-        return ans;
+        if(cnt1 > n/3)  ans.push_back(n1);
+        if(cnt2 > n/3)  ans.push_back(n2);
         
+        return ans;
     }
 };
-        // map<int,int> mp;
-        // vector<int> ans;
-
-        // for(auto it: nums){
-        //     mp[it]++;
-        //     if(mp[it] > n/3)    ans.push_back(it);
-        // }
